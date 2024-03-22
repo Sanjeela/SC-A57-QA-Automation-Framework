f9577750330956e0755d25cc0537ac0eb48384ca
Sanjeela <sanjeela.chitrakar@testpro.io>
2024-03-22 15:52:52 -0400

    L17
    L17

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,19 +9,60 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+
     @Test
     public void loginEmptyEmailPassword() {
 
 //      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        navigateToLoginPage();
+        clickSubmit();
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        Assert.assertEquals(driver.getCurrentUrl(),url);
 
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+    }
+
+    @Test
+    public void loginValidEmailPassword() {
+        //Steps
+        navigateToLoginPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+
+
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+       //Expected Result
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+
+
+    }
+
+    @Test
+    public void loginInvalidEmailValidPassword() {
+
+        //Steps
+        navigateToLoginPage();
+        provideEmail("invalid@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+       //Expected Result
+        Assert.assertTrue(avatarIcon.isDisplayed());
+
+
+    }
+
+    @Test
+    public void loginValidEmailNoPassword() {
+        //Steps
+        navigateToLoginPage();
+        provideEmail("demo@class.com");
+        clickSubmit();
+
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
     }
 }
+
